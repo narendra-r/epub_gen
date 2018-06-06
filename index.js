ab88ea3087845fdd0d6bd258046f2a51adf8824b
Narendrakumar Rangisetty <narendra.r@outlook.com>
2018-06-06 15:48:00 +0530

    create ePubs from all files under directory

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
 
 var Epub = require("epub-gen")
+var mammoth = require("mammoth");
+const path = require('path');
+var fs = require('fs');
+require('os').homedir()
+var recursive = require("recursive-readdir");
 
+var destinationPath = "/Users/kvanamac3/Downloads/Verse_doc_Chapter\ Wise/CH18";
+var ePubDirectory = destinationPath + "/ePubs"
+
+if (!fs.existsSync(ePubDirectory)){
+    fs.mkdirSync(ePubDirectory);
+}
+
+recursive(destinationPath, function (err, files) {
+    // `files` is an array of file paths
+    // console.log(files);
+    files.forEach(function (file) {
+        pareseDocument(file)
+    });
+})
+
+    function pareseDocument(file) {
+        console.log(path.parse(file).name);
+
+        var fileName = path.parse(file).name
+        mammoth.extractRawText({path: file})
+            .then(function(result){
+                var text = result.value; // The raw text
+                // console.log(text);
+                var messages = result.messages;
+                createePub(fileName, text)
+            })
+            .done();
+
+    }
+
+function createePub(name,text) {
     var option = {
-        title: "V1",
+        title: name,
         publisher: null,
         version: 3,
         appendChapterTitles: false,
@@ -10,12 +46,20 @@ var Epub = require("epub-gen")
         excludeFromToc: true,
         beforeToc: true,
         author: [""],
+        tocTitle: text,
+        customNcxTocTemplatePath: "./custome_toc.ncx.ejs",
         customHtmlTocTemplatePath:"./toc_template.xhtml.ejs",
         content: [
-            {
-                data: "<p>Alice was beginning to get very tired...</p>"
-            }
+            // {
+            //     data: "<pre> "+ text +"<pre>"
+            // }
         ]
     };
 
-    new Epub(option, "./v1.epub");
+     new Epub(option, ePubDirectory+"/"+name+".epub");
+}
+
+
+function getUserHome() {
+    return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+}
